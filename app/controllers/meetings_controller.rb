fb1e1c8c7650c0ce29c98dd7094b0fd8831369ad
Jesal Rathod <jesalrathod@qdata.io>
2020-03-06 20:17:31 +0530

    New Development for Meeting Module

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -163,7 +163,7 @@ class MeetingsController < ApplicationController
 
 private
   def meeting_params
-    params.require(:meeting).permit(:title, :location, :start_time, :duration, :start_date, :start_time_hour,
+    params.require(:meeting).permit(:title, :location, :start_time, :duration, :description, :results, :start_date, :start_time_hour,
       participants_attributes: [:email, :name, :invited, :attended, :user, :user_id, :meeting, :id])
   end
 end
